e7e4c536c87310441ef1ed17dd0985e7f7348f33
LeonWebSolution <leonwebdevv@gmail.com>
2023-08-15 15:04:30 +0300

    finish-animation

--- app/js/main.js
+++ app/js/main.js
@@ -163,6 +163,38 @@ $(function () {
         cursor.removeClass('hoverBtn');
     });
 
+    // Scroll reveal // 
+
+    window.sr = ScrollReveal()
+
+    sr.reveal('.animate-left', {
+        origin: 'left',
+        duration: 1000,
+        distance: '25rem',
+        delay: 500
+    });
+
+    sr.reveal('.animate-right', {
+        origin: 'right',
+        duration: 1000,
+        distance: '25rem',
+        delay: 500
+    });
+
+    sr.reveal('.animate-top', {
+        origin: 'top',
+        duration: 1000,
+        distance: '25rem',
+        delay: 600
+    });
+
+    sr.reveal('.animate-bottom', {
+        origin: 'bottom',
+        duration: 1000,
+        distance: '25rem',
+        delay: 600
+    });
+
 });
 
 
